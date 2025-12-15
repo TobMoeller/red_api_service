2cf66c3e8303999ea0119b6605c1c76b94d946d3
TobMoeller <tobiasmoellerw@t-online.de>
2025-12-15 16:50:16 +0100

    add better defaults to createOrder job

--- app/Jobs/RedProviderPortal/CreateOrder.php
+++ app/Jobs/RedProviderPortal/CreateOrder.php
@@ -5,6 +5,7 @@ namespace App\Jobs\RedProviderPortal;
 use App\Enums\Order\Status;
 use App\Models\Order;
 use App\Services\RedProviderPortal\Contracts\RedProviderClient;
+use DateTime;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
 use Illuminate\Support\Facades\DB;
@@ -13,9 +14,24 @@ class CreateOrder implements ShouldQueue
 {
     use Queueable;
 
+    /**
+     * TODO handle final job failure
+     *
+     * @var int
+     */
+    public $maxExceptions = 3;
+
+    /**
+     * @return array<int, int>
+     */
     public function backoff(): array
     {
         return [1, 10, 60];
+    }
+
+    public function retryUntil(): DateTime
+    {
+        return now()->plus(minutes: 60);
     }
 
     public function __construct(public Order $order)
