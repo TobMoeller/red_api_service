a3e8db5dc740564133aff4d7ba45b6384b86afc7
TobMoeller <tobiasmoellerw@t-online.de>
2025-12-15 16:43:35 +0100

    finish horizon installation

---
+++ app/Providers/HorizonServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Gate;
+use Laravel\Horizon\HorizonApplicationServiceProvider;
+
+class HorizonServiceProvider extends HorizonApplicationServiceProvider
+{
+    public function boot(): void
+    {
+        parent::boot();
+
+        // Horizon::routeSmsNotificationsTo('15556667777');
+        // Horizon::routeMailNotificationsTo('example@example.com');
+        // Horizon::routeSlackNotificationsTo('slack-webhook-url', '#channel');
+    }
+
+    protected function gate(): void
+    {
+        // TODO restrict horizon access
+        Gate::define('viewHorizon', function ($user = null) {
+            return true;
+            // return in_array(optional($user)->email, [
+            //     //
+            // ]);
+        });
+    }
+}
