7f316720d17f32b459ffd233fd96650f7f84a295
TobMoeller <tobiasmoellerw@t-online.de>
2025-12-15 14:49:12 +0100

    throttle api

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\Api\V1\OrderController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])
+Route::middleware(['auth:sanctum', 'throttle:api'])
     ->group(function () {
         Route::apiResource('/orders', OrderController::class)
             ->except(['update']);
